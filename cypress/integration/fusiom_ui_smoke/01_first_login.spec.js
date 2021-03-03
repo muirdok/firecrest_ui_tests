bdaff18269b4ce64660261caf25fe2b7496bfa06
muirdok <muirdokgmail.com>
2021-03-04 01:54:06 +0300

    lol kek

--- cypress/integration/fusiom_ui_smoke/01_first_login.spec.js
+++ cypress/integration/fusiom_ui_smoke/01_first_login.spec.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
 describe('My First Test', () => {
   it('clicking "type" shows the right headings', () => {
-    cy.visit('https://192.168.1.187:8457/')
+    cy.visit()
 
     //cy.pause()
     cy.wait(2000)
 
 
-    //cy.url().should('include', '/login')
-    //cy.get('#user')
-    //   .type('admin')
-    //   .should('have.value', 'admin')
-    //
-    // cy.get('#password')
-    //     .type('fusion')
-    //
-    // cy.get('.uwf-btn')
-    // .should('not.be.disabled')
-    // .click()
-    //
-    // cy.wait(5000)
+    cy.url().should('include', '/login')
+    cy.get('#user')
+      .type(Cypress.env('def_login'))
+      .should('have.value', Cypress.env('def_login'))
+
+    cy.get('#password')
+        .type(Cypress.env('def_pass'))
+
+    cy.get('.uwf-btn')
+    .should('not.be.disabled')
+    .click()
+
+    cy.wait(5000)
 
     cy.url().should('include', '/spog/dashboard')
 
